0ea265fc1896eefe19599ad2ad2315d31ca8a286
Mate Hegyhati <hegyhati.work@gmail.com>
2026-04-29 14:23:09 +0200

    First version

---
+++ foo.py
@@ -0,0 +1,14 @@
+def smaller (a, b):
+    return a  if a < b else b
+
+
+
+import unittest
+
+class SmallerTest(unittest.TestCase):
+
+    def testSmaller(self):
+        self.assertEqual(3, smaller(3,5))
+
+if __name__ == "__main__":
+    unittest.main()
