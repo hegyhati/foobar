060f802ede2af23cc9ab4b12ef0512be110b4ab7
Mate Hegyhati <hegyhati.work@gmail.com>
2026-04-29 14:34:00 +0200

    Add new tests

--- foo.py
+++ foo.py
@@ -9,6 +9,11 @@ class SmallerTest(unittest.TestCase):
 
     def testSmaller(self):
         self.assertEqual(3, smaller(3,5))
+        self.assertEqual(3, smaller(3,15))
+    
+    def testSmallerString(self):
+        self.assertEqual("3", smaller("3","5"))
+        self.assertEqual("3", smaller("3","15"))
 
 if __name__ == "__main__":
     unittest.main()
